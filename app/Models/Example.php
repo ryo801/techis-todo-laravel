7649662417621bbc98bdc31d562a6b69412dda0e
ryo801”git git version 2.37.1 (Apple Git-137.1)git config --global user.name ryo801” <153968719+ryo801@users.noreply.github.com>
2024-01-27 14:17:39 +0900

    first commit

---
+++ app/Models/Example.php
@@ -0,0 +1,26 @@
+<?php
+
+class ExampleController extends controller
+{
+ public function hoge()
+ {
+   //必要な処理を書く
+
+   return view('hoge');
+ }
+
+   public function fuga()
+ {
+   //必要な処理を書く
+
+   return view('fuga');
+ }
+
+   public function piyo()
+ {
+   //必要な処理を書く
+
+   return view('piyo');
+ }
+}
+?>
