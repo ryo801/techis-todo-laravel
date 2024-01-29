2da13fffd0d7ef12408ed8f1e8f896221d6334e2
ryo801”git git version 2.37.1 (Apple Git-137.1)git config --global user.name ryo801” <153968719+ryo801@users.noreply.github.com>
2024-01-29 16:53:48 +0900

    修正２

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -23,7 +23,7 @@ class AppServiceProvider extends ServiceProvider
     {
         //
         if(\App::environment(['production'])){
-            \URL::forceScheme(https);
+            \URL::forceScheme('https');
         }
     }
 }
